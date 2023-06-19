d77409fba5748ebf6ea48da8608aaec8696c5ae6
nruxshod07 <nezametdinovruxsod@gmail.com>
2023-06-19 14:14:53 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+let season = prompt()
+if (season <=2) {
+    alert("winter")
+} else if (season <= 5) {
+    alert("spring")
+} else if (season <=8) {
+    alert("summer")
+} else if (season <= 11) {
+    alert("autumn")
+} else if (season == 12) {
+    alert("winter")
+} else (
+    alert("error")
+)
+
+let month = prompt()
+if (month <= 10) {
+    alert("first decade")
+}
+else if (month <= 20) {
+    alert("second decade")
+}
+else if (month <= 31) {
+    alert("third decade")
+}
+else (
+    alert("error")
+)
